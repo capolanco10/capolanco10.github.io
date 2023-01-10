4bce68d6687ceccd49a3676d3b8dc7a485a93648
CARVAJAL\carcorpo <carlos.cortesp@carvajal.com>
2023-01-10 12:21:40 -0500

    actualizacion portafolio portal web github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://capolanco10.github.io',
-    base: '/capolanco10.github.io'
+    base: '/'
 });
