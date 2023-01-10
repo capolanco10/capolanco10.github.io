929ead91e3ba6d2ffa2a55f4ca591c53cbd796d7
CARVAJAL\carcorpo <carlos.cortesp@carvajal.com>
2023-01-10 15:45:50 -0500

    new templae

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,73 +1,33 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
 import { defineConfig } from 'astro/config';
-
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import image from '@astrojs/image';
-import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
-import compress from 'astro-compress';
+import robotsTxt from 'astro-robots-txt';
+import { astroImageTools } from 'astro-imagetools';
 
-import { remarkReadingTime } from './src/utils/frontmatter.mjs';
-import { SITE } from './src/config.mjs';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const whenExternalScripts = (items = []) =>
-  SITE.googleAnalyticsId ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
-
+// https://astro.build/config
 export default defineConfig({
-  site: SITE.origin,
-  base: SITE.basePathname,
-  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
-
-  output: 'static',
-
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    mdx(),
-
-    ...whenExternalScripts(() =>
-      partytown({
-        config: { forward: ['dataLayer.push'] },
-      })
-    ),
-
-    compress({
-      css: true,
-      html: true,
-      img: false,
-      js: true,
-      svg: false,
-
-      logger: 1,
-    }),
-  ],
-
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-    extendDefaultPlugins: true,
+  // base: '.', // Set a path prefix.
+  site: 'https://capolanco10.github.io/', // Use to generate your sitemap and canonical URLs in your final build.
+  // Important!
+  // Only official '@astrojs/*' integrations are currently supported by Astro.
+  // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
+  // with 'astro build' command.
+  experimental: {
+    integrations: true,
   },
-
-  vite: {
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, './src'),
-      },
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'monokai',
     },
   },
-
-  experimental: {
-    contentCollections: true,
-  },
+  integrations: [
+    react(),
+    tailwind({}),
+    sitemap(),
+    robotsTxt(),
+    astroImageTools,
+  ],
 });
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,29 +1,20 @@
 import rss from '@astrojs/rss';
 
-import { SITE, BLOG } from '~/config.mjs';
-import { fetchPosts } from '~/utils/blog';
-import { getPermalink } from '~/utils/permalinks';
+import { AppConfig } from '@/utils/AppConfig';
 
-export const get = async () => {
-  if (BLOG.disabled) {
-    return new Response(null, {
-      status: 404,
-      statusText: 'Not found',
-    });
-  }
-
-  const posts = await fetchPosts();
-
-  return rss({
-    title: `${SITE.name}’s Blog`,
-    description: SITE.description,
+export const get = () =>
+  rss({
+    // `<title>` field in output xml
+    title: AppConfig.title,
+    // `<description>` field in output xml
+    description: AppConfig.description,
+    // base URL for RSS <item> links
+    // SITE will use "site" from your project's astro.config.
     site: import.meta.env.SITE,
-
-    items: posts.map((post) => ({
-      link: getPermalink(post.slug, 'post'),
-      title: post.title,
-      description: post.description,
-      pubDate: post.publishDate,
-    })),
+    // list of `<item>`s in output xml
+    // simple example: generate items for every md file in /src/pages
+    // see "Generating items" section for required frontmatter and advanced use cases
+    items: import.meta.glob('./**/*.md'),
+    // (optional) inject custom xml
+    customData: `<language>en-us</language>`,
   });
-};
